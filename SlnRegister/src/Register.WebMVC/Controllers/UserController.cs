678f436b42ea14c9badd2fe8f512763ab7fb7752
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-10 20:34:55 -0300

    Feat: Adicionando ao Controller o Index, Create e Edit

--- SlnRegister/src/Register.WebMVC/Controllers/UserController.cs
+++ SlnRegister/src/Register.WebMVC/Controllers/UserController.cs
@@ -1,12 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Register.Domain.Contracts.Services;
+using Register.Domain.DTO;
 
 namespace Register.WebMVC.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(_service.GetAll());
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("id, personId, person, username, password, email, userType")] UserDTO user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _service.Save(user) > 0) return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            if(id == null) return NotFound();
+
+            var user = await _service.GetById(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, [Bind("id, personId, person, username, password, email, userType")] UserDTO user)
+        {
+            if (!(id == user.id)) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                if (await _service.Save(user) > 0) return RedirectToAction(nameof(Index));
+            }
+            return View(user);
         }
     }
 }
