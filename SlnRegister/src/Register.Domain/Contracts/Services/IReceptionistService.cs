ed2408b7b89b146199317590b7824897577e83a1
coramori <coramori@gmail.com>
2023-02-10 15:02:37 -0300

    Feat: Add Receptionist | Fix: Entities IDs, Doctor e Patient DTOs

---
+++ SlnRegister/src/Register.Domain/Contracts/Services/IReceptionistService.cs
@@ -0,0 +1,14 @@
+﻿using Register.Domain.DTO;
+using Register.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Register.Domain.Contracts.Services
+{
+    public interface IReceptionistService : IBaseService<ReceptionistDTO>
+    {
+    }
+}
