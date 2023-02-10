51eca4f84db4fd3688a6215fb1423646eb6a9268
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-09 22:24:08 -0300

    feat: User, UserDTO, PersonType (ENUM), arrumando o ConditionDTO

---
+++ SlnRegister/src/Register.Domain/Enums/PersonTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Register.Domain.Enums
+{
+    public enum PersonTypeEnum
+    {
+        Doctor = 1,
+        Patient = 2,
+        Receptionist = 0
+    }
+}
