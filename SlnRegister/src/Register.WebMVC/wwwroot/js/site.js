9ee04238ccad6136d9bb848759a4ed1eaa5eee0a
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-14 22:20:08 -0300

    Feat: Paginação Person

--- SlnRegister/src/Register.WebMVC/wwwroot/js/site.js
+++ SlnRegister/src/Register.WebMVC/wwwroot/js/site.js
@@ -1,6 +1,7 @@
 ﻿// Modal do DELETE
 $(document).ready(() => {
     console.log("Cadastro Hospitalar - Script Loaded")
+    getDataTable('#table-person');
 });
 
 liveToastMessage = (message, origin) => {
@@ -25,4 +26,37 @@ msgModalMessage = (message, callback) => {
 
 closeModalMessage = () => {
     $('#msgModal').modal('hide');
+}
+
+/* Paginação */
+
+function getDataTable(id) {
+    $(id).DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
 }
